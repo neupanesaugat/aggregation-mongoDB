e0c3a12b52b4fa62370e2f4ac04dca04a2cbd8b1
neupanesaugat <saugatneupane50@gmail.com>
2024-07-08 16:33:38 +0545

    Lookup in MongoDB

--- lookup.mongodb.js
+++ lookup.mongodb.js
@@ -66,32 +66,76 @@ use("kec-crud");
 //   },
 // ]);
 
-db.person.aggregate([
-  {
-    $match: {
-      firstName: "Rohan",
-    },
-  },
-  {
-    $lookup: {
-      from: "vehicle", //? always check in compass
-      localField: "_id",
-      foreignField: "ownerId",
-      as: "vehicleDetails",
-    },
-  },
-  {
-    $project: {
-      ownerFullName: { $concat: ["$firstName", " ", "$lastName"] },
-      vehicleModel: { $arrayElemAt: ["$vehicleDetails.model", 0] }, //? $arrayElemAt can be used as replacement of $first (here 0 refers to the first element of array)
-      vehicleBrand: { $first: "$vehicleDetails.brand" },
-      _id: 0,
-    },
-  },
-  {
-    $project: {
-      ownerFullName: 1,
-      vehicleName: { $concat: ["$vehicleBrand", " ", "$vehicleModel"] },
-    },
-  },
-]);
+// db.person.aggregate([
+//   {
+//     $match: {
+//       firstName: "Rohan",
+//     },
+//   },
+//   {
+//     $lookup: {
+//       from: "vehicle", //? always check in compass
+//       localField: "_id",
+//       foreignField: "ownerId",
+//       as: "vehicleDetails",
+//     },
+//   },
+//   {
+//     $project: {
+//       ownerFullName: { $concat: ["$firstName", " ", "$lastName"] },
+//       vehicleModel: { $arrayElemAt: ["$vehicleDetails.model", 0] }, //? $arrayElemAt can be used as replacement of $first (here 0 refers to the first element of array)
+//       vehicleBrand: { $first: "$vehicleDetails.brand" },
+//       _id: 0,
+//     },
+//   },
+//   {
+//     $project: {
+//       ownerFullName: 1,
+//       vehicleName: { $concat: ["$vehicleBrand", " ", "$vehicleModel"] },
+//     },
+//   },
+// ]);
+
+// db.vehicle.insertOne({
+//   model: "Y",
+//   brand: "Tesla",
+//   builtYear: 2020,
+//   ownerId: ObjectId("668bb313c0e5c0cddeb847ce"),
+// });
+
+// db.person.aggregate([
+//   {
+//     $match: {
+//       firstName: "Enish",
+//     },
+//   },
+//   {
+//     $lookup: {
+//       localField: "_id",
+//       from: "vehicle",
+//       foreignField: "ownerId",
+//       as: "vehicleData",
+//     },
+//   },
+//   {
+//     $project: {
+//       ownerFullName: { $concat: ["$firstName", " ", "$lastName"] },
+//       firstVehicleModel: { $first: "$vehicleData.model" },
+//       firstVehicleBrand: { $first: "$vehicleData.brand" },
+//       secondVehicleModel: { $last: "$vehicleData.model" },
+//       secondVehicleBrand: { $last: "$vehicleData.brand" },
+//     },
+//   },
+//   {
+//     $project: {
+//       ownerFullName: 1,
+//       firstVehicleName: {
+//         $concat: ["$firstVehicleBrand", " ", "$firstVehicleModel"],
+//       },
+//       secondVehicleName: {
+//         $concat: ["$secondVehicleBrand", " ", "$secondVehicleModel"],
+//       },
+//       _id: 0,
+//     },
+//   },
+// ]);
